b2e9f25104db4535bd8c9455cfbcf69b6eb7e902
Eliott <eliott.tenn@student.tdsb.on.ca>
2025-03-27 15:07:03 -0400

    imports?

--- A4/src/App.jsx
+++ A4/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ambiguousCase from './components'
+import heronsFormula from './components'
+import newtownsmethod from './components'
+import polynomialFunction from './components'
 
 
 
