919b9d34639bc60ca4109ba55e543179a6883491
Eliott <eat.ten10@gmail.com>
2025-03-28 22:07:55 -0400

    Trouble shot

--- A4/src/App.jsx
+++ A4/src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ambig from './components'
-import heron from './components'
-import newton from './components'
-import poly from './components'
+// import ambig from '.components'
+// import heron from './components'
+// import newton from './components'
+// import poly from './components'
 
 function App() {
 
