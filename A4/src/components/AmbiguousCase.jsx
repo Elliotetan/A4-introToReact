0a72a55e1937d92897927205938182640c54d903
Eliott <eliott.tenn@student.tdsb.on.ca>
2025-04-01 14:31:14 -0400

    Camel Case

--- A4/src/components/AmbiguousCase.jsx
+++ A4/src/components/AmbiguousCase.jsx
@@ -12,11 +12,10 @@ function Ambig() {
         e.preventDefault();  // Prevent default form submission
 
         const angle = Number(angleA);
-        const A = Number(sideA);
-        const B = Number(sideB);
-        const hy = B * Math.sin(angle * (Math.PI / 180)); // Calculate height (using sine of the angle)
+        const A = Number(sideA); //camelCase
+        const B = Number(sideB); //camelCase
+        const hy = B * Math.sin(angle * (Math.PI / 180));
 
-        // Check the angle and side conditions for ambiguous cases
         if (0 < angle && angle <= 90) {
             if (A < hy) {
                 setResult("No triangle");
@@ -42,11 +41,11 @@ function Ambig() {
         <form className="container" onSubmit={ambigCase}>
             <h1 className="header">Ambiguous Case</h1>
             <label>Angle A:</label>
-            <input type="number" value={angleA} onChange={(event) => setA(Number(event.target.value))} required/>
+            <input type="number" value={angleA} onChange={(event) => setA(Number(event.target.value))} required />
             <label>Side a:</label>
-            <input type="number" value={sideA} onChange={(event) => setB(Number(event.target.value))} required/>
+            <input type="number" value={sideA} onChange={(event) => setB(Number(event.target.value))} required />
             <label>Side b:</label>
-            <input type="number" value={sideB} onChange={(event) => setC(Number(event.target.value))} required/>
+            <input type="number" value={sideB} onChange={(event) => setC(Number(event.target.value))} required />
             <button className="calculate" type="submit" id="ambiguousSubmit">Calculate</button>
             <label>Triangle Type (Result)</label>
             <input type="text" id="amb-result" name="amb-result" value={result} readOnly />
